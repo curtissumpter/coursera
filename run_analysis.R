62b174ee8d5376faeadf1a9c277ea9f97eca938e
Arthur Sumpter <curtis.sumpter@gmail.com>
2014-12-13 20:21:42 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
-download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile="galaxyProject1.zip", method="curl");
+## download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile="galaxyProject1.zip", method="curl");
 
-unzip("galaxyProject1.zip");
+## unzip("galaxyProject1.zip");
 
 setwd("./UCI HAR Dataset");
 
